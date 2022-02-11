69a0235c4aae16a4db945ef6e13c57d99262c348
chaichontat <chaichontat@users.noreply.github.com>
2022-02-11 02:58:12 +0000

    deploy: 6134020fd23304a5ee044c60f11d6fd4d4c5591c

---
+++ internal/pages/main_auto.svelte-10c2d4ef.js
@@ -0,0 +1,1 @@
+import"../chunks/vendor-ffd40cc1.js";import{M as p}from"../chunks/main_auto-36acc906.js";import"../chunks/store-d7058b45.js";export{p as default};
--- internal/pages/main_auto.svelte-5359d5da.js
+++
@@ -1,1 +0,0 @@
-import"../chunks/vendor-d2ee3b07.js";import"../chunks/store-f06ce2a0.js";import{M as r}from"../chunks/main_auto-bc28c2b1.js";export{r as default};
