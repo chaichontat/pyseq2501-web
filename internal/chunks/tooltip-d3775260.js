812dcc74079f6b0401a0ebd655ab945eb4ecc46f
chaichontat <chaichontat@users.noreply.github.com>
2022-03-06 16:04:53 +0000

    deploy: f7a9c0845c6dac746046cfe1b79f995fd9ed43f4

--- internal/chunks/tooltip-d3775260.js
+++
@@ -1,1 +0,0 @@
-import{a6 as i}from"./vendor-791ea25a.js";var s=(t,r)=>{t.setAttribute("aria-label",r),t.title="";const a=i(t,{content:r,delay:[100,0]});return{update:e=>a.setContent(e),destroy:()=>a.destroy()}};export{s as t};
